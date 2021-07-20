56822c47f72acf596ec121dce36246ea40344da4
이동아 <christar99@naver.com>
2021-07-20 13:01:18 +0900

    11.14.1 업데이트

--- src/API.js
+++ src/API.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const versionAPI = axios('https://ddragon.leagueoflegends.com/api/versions.json');
 
-export const itemAPI = axios('https://ddragon.leagueoflegends.com/cdn/11.13.1/data/ko_KR/item.json');
+export const itemAPI = axios('https://ddragon.leagueoflegends.com/cdn/11.14.1/data/ko_KR/item.json');
 
 const championList = axios('http://ddragon.leagueoflegends.com/cdn/11.14.1/data/ko_KR/champion.json');
 
