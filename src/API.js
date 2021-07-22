019a6e9779a656b62aa307922a8644dce5a52c75
이동아 <christar99@naver.com>
2021-07-22 15:54:53 +0900

    http=> https 변경

--- src/API.js
+++ src/API.js
@@ -2,14 +2,14 @@ import axios from 'axios';
 
 export const versionAPI = axios('https://ddragon.leagueoflegends.com/api/versions.json');
 
-export const itemAPI = axios('https://ddragon.leagueoflegends.com/cdn/11.14.1/data/ko_KR/item.json');
+export const itemAPI = axios('https://ddragon.leagueoflegends.com/cdn/11.15.1/data/ko_KR/item.json');
 
-const championList = axios('http://ddragon.leagueoflegends.com/cdn/11.14.1/data/ko_KR/champion.json');
+const championList = axios('https://ddragon.leagueoflegends.com/cdn/11.15.1/data/ko_KR/champion.json');
 
 export const ChampionAPI = async () => {
     const championName = Object.keys((await championList).data.data);
 
-    const champions = championName.map(champion => axios(`https://ddragon.leagueoflegends.com/cdn/11.14.1/data/ko_KR/champion/${champion}.json`));
+    const champions = championName.map(champion => axios(`https://ddragon.leagueoflegends.com/cdn/11.15.1/data/ko_KR/champion/${champion}.json`));
 
     let championState = [];
     for(let champion of champions) {
