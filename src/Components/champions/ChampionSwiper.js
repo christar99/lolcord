99fcfecc7a8d54639c605bc6138988a50fdf2aa1
이동아 <christar99@naver.com>
2021-08-01 20:30:55 +0900

    skin 네비게이션 삭제, slideperview 로 총길이변경

--- src/Components/champions/ChampionSwiper.js
+++ src/Components/champions/ChampionSwiper.js
@@ -267,7 +267,7 @@ const ChampionSwiper = ({ champions, clickedChampion }) => {
     return (
         <SwiperContainer>
             <Swiper
-                slidesPerView={12}
+                slidesPerView={13}
                 spaceBetween={10}
                 navigation
                 scrollbar={{ "draggable": true }}
